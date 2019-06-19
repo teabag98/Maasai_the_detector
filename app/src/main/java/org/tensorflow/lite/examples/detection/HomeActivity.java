2070e42c9884ab743f9cef6f83daa5c35e5d986c
eric <eric.kingoo2014@gmail.com>
2019-06-19 15:33:45 +0300

    debug android xml

--- app/src/main/java/org/tensorflow/lite/examples/detection/HomeActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/HomeActivity.java
@@ -1,5 +1,35 @@
 package org.tensorflow.lite.examples.detection;
 
-public class HomeActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.detectButton)
+    Button mDetectButton;
+    @BindView(R.id.uploadButton) Button mUploadButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+        mUploadButton.setOnClickListener(this);
+        mDetectButton.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        if(v == mDetectButton){
+
+            Intent intent = new Intent(HomeActivity.this, DetectorActivity.class);
+            startActivity(intent);
+        }
+
+
+    }
 }
